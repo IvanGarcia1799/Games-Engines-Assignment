a4bcc1c71aa4c001c65ab3f8d1e005716c91050b
unknown <ivan.agustin.garcia17@gmail.com>
2020-12-24 14:38:36 +0000

    updated existing orbs to visualize sound. WIP

--- Rythm Fighter/Assets/Orb.cs
+++ Rythm Fighter/Assets/Orb.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Orb : MonoBehaviour
 {
-    public int elements = 20;
+    public int elements = 50;
+    public int map = 0;
     public float radius = 3;
+    public float scale = 10;
+    List<GameObject> orbList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         float theta = Mathf.PI * 2.0f / (float) elements;
+        map = (int)(Mathf.Floor(AudioAnalyzer.frameSize/elements));
         for(int i = 0 ; i < elements ; i ++)
         {
             GameObject sp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -17,13 +21,20 @@ public class Orb : MonoBehaviour
             sp.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
             Vector3 pos = new Vector3(Mathf.Sin(theta * i) * radius, 0, Mathf.Cos(theta * i) * radius);
             sp.transform.position = transform.TransformPoint(pos);
-            
+            orbList.Add(sp);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int curs = 0;
+        for(int i = 0 ; i < orbList.Count ; i ++)
+        {
+            Vector3 lscale = orbList[i].transform.localScale; 
+            lscale.y = Mathf.Lerp(lscale.y, 0.2f + (Mathf.Abs(AudioAnalyzer.spectrum[curs]) * scale), Time.deltaTime * 10);
+            orbList[i].transform.localScale = lscale;
+            curs += map;
+        }
     }
 }
